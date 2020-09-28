777c0cfe648685a2c4243081d4712824392f9ee1
simranramtekkar <71745013+simranramtekkar@users.noreply.github.com>
2020-09-28 17:25:49 +0530

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Style.css';
+
+
+const Navbar =()=>{
+	return (
+
+
+<div className="navbar">
+<ul>
+  <li className ="margin">LOGO</li>
+  <li><link to ="/">Home</link></li>
+  <li><link to ="/Task">Task</link></li>
+  <li><link to ="/User">User</link></li>
+ </ul>
+
+
+
+
+</div>
+
+
+		);
+} ;
+export default Navbar
